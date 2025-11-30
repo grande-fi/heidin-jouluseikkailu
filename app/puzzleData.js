77a77e7e241ed8fa74c4b3a66510ee1e3ec4120e
Karl Granström <karl.granstrom@gmail.com>
2025-11-30 20:35:59 +0200

    kyssäri päivitykset
    Finaali kyssärit setti 1

--- app/puzzleData.js
+++ app/puzzleData.js
@@ -1,87 +1,87 @@
 // app/puzzleData.js
 export const puzzles = [
   { 
-    question: "Mikä on 1+1?", 
+    question: "Kuinka monta sormea sinulla on kädessä?", 
     answer: "5",
-    correctFeedback: "Oikein! Loistava alku!",
-    wrongFeedback: "Ei ihan. Yritä uudelleen!"
+    correctFeedback: "Oikein! Loistava alku! Laita N 5 muistiin!",
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
-    question: "Mikä on pääkaupunki Suomessa?", 
+    question: "Mene kätkölle CGBDCYG. Mistä alkaen klo kätkö vaatii parkkikiekon?", 
     answer: "6",
-    correctFeedback: "Aivan oikein! Helsinki on oikea vastaus!",
-    wrongFeedback: "Ei oikein. Mieti vielä!"
+    correctFeedback: "Rock Rakas! laita 6° talteen edellisen numeron perään.",
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
-    question: "Mikä onn koordinaatti N?", 
+    question: "10m kätköstä etelään löytyy numerosarja: 347X Mikä on X?", 
     answer: "5",
-    correctFeedback: "Täydellinen! Olet oikeilla jäljillä!",
-    wrongFeedback: "Tarkista koordinaatit uudelleen."
+    correctFeedback: "Täydellistä! Tätä menoa isi tulee ja tekee temput sulle ❤️ Muistiin 5.",
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
-    question: "Mikä on koordinaatti E?", 
+    question: "Kätkön lähettyviltä löytyy keltaisia autonlatausasemia, kuinka monta kappaletta?", 
     answer: "6",
-    correctFeedback: "Erinomaista! Koordinaatti selvä!",
-    wrongFeedback: "Ei ihan oikein. Yritä uudelleen!"
+    correctFeedback: "Erinomaista! Kohta mennään kyllä pusikkoon... vai kuusikkoon? Kuusi kuitenki muistiin 😘",
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
-    question: "Mikä väri tulee kun sekoitat punaisen ja sinisen?", 
+    question: "Naateista N60° 7.720 E24° 41.088 löytyy numerosarja: 123633X. Mikä on X?", 
     answer: "7",
-    correctFeedback: "Oikein! Violetti on oikea vastaus!",
-    wrongFeedback: "Ei oikein. Ajattele värejä!"
+    correctFeedback: "Nice! Ja tästä saatiin mumero 7",
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
-    question: "Mikä eläin sanoo 'muu'?", 
+    question: "Sitten suunataan kätkölle GCB5X33. Kätkölle on nopeusrajoitus 1Xkm/h. Kuinka paljon on X?", 
     answer: "0",
-    correctFeedback: "Oikein! Lehmä sanoo muu!",
-    wrongFeedback: "Ei se ollut se eläin. Yritä uudelleen!"
+    correctFeedback: "Oikein! Nolla ei oo turha mutta ylös sekin.",
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
-    question: "Mikä on Suomen rahayksikkö?", 
+    question: "Kätkön lähistöllä on kaappeja ja yhdessä niistä numerosarja J156X50. Taas etsitään X:ää.", 
     answer: "3",
-    correctFeedback: "Täsmälleen! Euro on oikea vastaus!",
-    wrongFeedback: "Ei ihan. Mieti rahaa!"
+    correctFeedback: "Täsmälleen! 3 ylös ja seikkailujatkuu (ehkä makkarin kautta?)",
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
-    question: "Mikä on joulupukin asuinpaikka?", 
+    question: "", 
     answer: "2",
     correctFeedback: "Oikein! Korvatunturi on oikea paikka!",
-    wrongFeedback: "Ei oikein. Mieti Lappia!"
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
     question: "Mikä on 5*5?", 
     answer: "4",
     correctFeedback: "Loistavaa! 25 on oikein!",
-    wrongFeedback: "Ei ihan. Laske uudelleen!"
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
     question: "Mikä on maapallon suurin valtameri?", 
     answer: "0",
     correctFeedback: "Oikein! Tyyni valtameri on suurin!",
-    wrongFeedback: "Ei se. Mieti isoimpia valtameriä!"
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
     question: "Mikä on Suomen kansalliseläin?", 
     answer: "6",
     correctFeedback: "Täydellinen! Karhu on oikea vastaus!",
-    wrongFeedback: "Ei oikein. Mieti metsän eläimiä!"
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
     question: "Mikä luku tulee seuraavaksi: 2, 4, 6, ...?", 
     answer: "4",
     correctFeedback: "Oikein! 8 on seuraava!",
-    wrongFeedback: "Ei ihan. Katso sarjaa uudelleen!"
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
     question: "Mikä on  pääjoki?", 
     answer: "7",
     correctFeedback: "Erinomaista! Daugava on oikea joki!",
-    wrongFeedback: "Ei se. Mieti Rigan jokia!"
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
   { 
     question: "Mikä väri tulee kun sekoitat keltaisen ja sinisen?", 
     answer: "5",
     correctFeedback: "Loistavaa! Vihreä on oikein!",
-    wrongFeedback: "Ei oikein. Mieti värejä!"
+    wrongFeedback: "Ei ihan. Yritä uudelleen tai saat suukon!"
   },
 ];
